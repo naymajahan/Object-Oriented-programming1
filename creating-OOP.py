5bbad0095919ef4aef97132a42628e10e4faf6ec
naymajahan <naymajahanshoshe@gmail.com>
2021-04-29 11:16:49 +0600

    creating oop

---
+++ creating-OOP.py
@@ -0,0 +1,24 @@
+#### object oriented programming in python
+
+class StoryBook:
+    pass
+##  Creating an instance / object of the storybook class
+book1 = StoryBook()
+book2 = StoryBook()
+
+##### instance variable
+
+book1.name = 'NowkaDubi'
+book1.price = 250
+book1.authorName = 'Rabindranath Tagor'
+book1.Authoeborn = 1864
+
+
+book2.name = 'ChorittroHin'
+book2.price = 450
+book2.authorName = "Sharat Chandra "
+book2.Authorborne = 1820
+
+print(book1.name)
+print(book2.name)
+
